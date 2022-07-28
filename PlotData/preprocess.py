77348b57cb7a8020973c021032c07e2f2afd932a
VitaliiUr <vitaliyurbanevich@gmail.com>
2022-07-28 15:11:14 +0200

    New totcross data

--- PlotData/preprocess.py
+++ PlotData/preprocess.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm import tqdm
 
 FORCES = ["LO", "NLO", "N2LO", "N3LO", "N4LO", "N4LO+"]
-src = "Deuteron/data/"
+src = "../../../Deuteron/PRODUCTION1_DEUTERON_2022/output/"
 
 
 def read_data(fname):
@@ -18,42 +18,33 @@ def read_data(fname):
 
 
 def read_chiral(fname):
-    add = 0
-    skpfoot = 0
-    if "dat1" in fname:
-        add = 1
-        skpfoot = 6
     with open(src + fname, "r") as f:
-        cols = f.readlines()[0+add]
+        cols = f.readlines()[int("dat1" in fname)]
         cols = [c.strip() for c in cols.split("#")[-1].split(",")]
-    df = pd.read_csv(src + fname, skiprows=1+add, delimiter=" ",
-                     skipinitialspace=True, skipfooter=skpfoot, engine='python')
-    df.columns = cols
+    df = pd.read_csv(src + fname, delim_whitespace=True,
+                     skipinitialspace=True, engine='python',
+                     comment="#", skip_blank_lines=True,
+                     names=cols)
     df["angle"] = (df["THCM"]/np.pi*180).round(2)
     df["CUTOFF"] = 350 + 50*int(fname.split(".")[3])
     ostat = int(fname.split(".")[2])
     df["FORCE"] = FORCES[ostat]
-    # if FORCES[ostat] == "N4LO+":
-    #     print (fname, 350 + 50*int(fname.split(".")[3]), FORCES[ostat])
     return df
 
 
 def read_av18(fname):
-    add = 0
-    skpfoot = 0
-    if "dat1" in fname:
-        add = 1
-        skpfoot = 6
     with open(src + fname, "r") as f:
-        cols = f.readlines()[0+add]
+        cols = f.readlines()[int("dat1" in fname)]
         cols = [c.strip() for c in cols.split("#")[-1].split(",")]
-    df = pd.read_csv(src + fname, skiprows=1+add, delimiter=" ",
-                     skipinitialspace=True, skipfooter=skpfoot, engine='python')
-    df.columns = cols
+    df = pd.read_csv(src + fname, delim_whitespace=True,
+                     skipinitialspace=True, engine='python',
+                     comment="#", skip_blank_lines=True,
+                     names=cols)
     df["angle"] = (df["THCM"]/np.pi*180).round(2)
     df["CUTOFF"] = 0
     df["FORCE"] = "AV18"
     return df
+
 
 if __name__ == "__main__":
     energies = set(map(lambda x: int(x.split(".")[-1].rstrip("mev")),
@@ -68,23 +59,29 @@ if __name__ == "__main__":
         df3 = pd.concat([read_data(fname) for fname in data if "dat3" in fname])
         df4 = pd.concat([read_data(fname) for fname in data if "dat4" in fname])
         df5 = pd.concat([read_data(fname) for fname in data if "dat5" in fname])
-        
-        df = pd.merge(left=df1, right=df2.drop(columns=["THCM"]), on=["angle", "CUTOFF", "FORCE", "WAVE"])
-        df = pd.merge(left=df, right=df3.drop(columns=["THCM"]), on=["angle", "CUTOFF", "FORCE", "WAVE"])
-        df = pd.merge(left=df, right=df4.drop(columns=["THCM"]), on=["angle", "CUTOFF", "FORCE", "WAVE"])
-        df = pd.merge(left=df, right=df5.drop(columns=["THCM"]), on=["angle", "CUTOFF", "FORCE", "WAVE"])
+
+        df = pd.merge(left=df1, right=df2.drop(columns=["THCM"]),
+                      on=["angle", "CUTOFF", "FORCE", "WAVE"])
+        df = pd.merge(left=df, right=df3.drop(columns=["THCM"]),
+                      on=["angle", "CUTOFF", "FORCE", "WAVE"])
+        df = pd.merge(left=df, right=df4.drop(columns=["THCM"]),
+                      on=["angle", "CUTOFF", "FORCE", "WAVE"])
+        df = pd.merge(left=df, right=df5.drop(columns=["THCM"]),
+                      on=["angle", "CUTOFF", "FORCE", "WAVE"])
         df["Energy"] = energy
         df_all = df_all.append(df, ignore_index=True)
 
     df_all.to_csv("Deuteron/deuteron_all_data.csv", index=False)
 
-    src = "Deuteron/ExpData/DiffCross"
+    src = "Deuteron/ExpData/DiffCross/"
     df_exp = pd.DataFrame()
     for f in os.listdir(src):
-        if not os.path.isfile(src+f) or not f.endswith(".dat"):
+        if not os.path.isfile(src+f):
             print(f"not a file {src+f}")
             continue
-        print(f)
+        if not f.endswith(".dat"):
+            print(f"wrong file {src+f}")
+            continue
         df1 = pd.read_csv(src+f, delim_whitespace=True, comment="#",
                           skip_blank_lines=True,
                           header=None, engine='python',
@@ -94,5 +91,20 @@ if __name__ == "__main__":
             if str(energy) in f:
                 df1["energy"] = energy
                 break
-        df_exp = df_exp.append(df1)
-    df_exp.to_csv("Deuteron/deuteron_all_exp.csv", index=False)
+        df_exp = pd.concat((df_exp, df1), ignore_index=True)
+    df_exp.to_csv("Deuteron/deuteron_exp_diffcross.csv", index=False)
+
+    src = "./Deuteron/ExpData/TotalCross/"
+    df_exp_tot = pd.DataFrame()
+    for f in os.listdir(src):
+        if not os.path.isfile(src+f):
+            print(f"not a file {src+f}")
+            continue
+        if not f.endswith(".csv"):
+            print(f"wrong file {src+f}")
+            continue
+        df1 = pd.read_csv(src+f, comment="#",
+                          skip_blank_lines=True,engine='python')
+        df1["fname"] = f
+        df_exp_tot = pd.concat((df_exp_tot, df1), ignore_index=True)
+    df_exp_tot.to_csv("Deuteron/deuteron_exp_totcross.csv", index=False)
